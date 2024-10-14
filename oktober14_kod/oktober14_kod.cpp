4ec40830b3509ed85bf9589685da1c75d531e6ab
234875 <234875@KB37-309-C06>
2024-10-14 11:17:22 +0300

    Добавьте файлы проекта.

---
+++ oktober14_kod/oktober14_kod.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string>
+
+using namespace std;
+
+void _kalk(string a, float b)
+{
+    cout << a << " " << b << endl;
+}
+float _Sum(float a, float b)
+{
+    return a + b; 
+}
+float Kvadr(float a)
+{
+    return a * a;
+}
+float _Del(float a, float b)
+{
+    if (b != 0)
+    { 
+        return a / b;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    _kalk("Vesh_chislo", 2.6);
+    _kalk("Summa = ", _Sum(2.0, 3.0));
+    _kalk("Kvadr = ", Kvadr(9.0));
+    float a = 9.0, b = 12.0;
+
+    cout << "Summa = " << a << " and " << b << " = " << _Sum(a, b) << endl;
+    cout << " Del = " << a << " and " <<  b << " = " << _Del(a, b) << endl;
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+}
